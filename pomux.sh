e665b126460fb6fa7ae4e336748f1ee7d577f05c
Carlo Caione <carlo@caione.org>
2014-10-21 17:17:07 +0200

    Code refactoring
    Signed-off-by: Carlo Caione <carlo@caione.org>

--- pomux.sh
+++ pomux.sh
@@ -1,39 +1,38 @@
 #!/bin/sh
 
-t_m_session=1
-t_m_short_break=1
-t_m_long_break=1
+time_session_min=1
+time_short_break_min=1
+time_long_break_min=1
 
-c_session="#[fg=mycolor,bg=mycolor]#[fg=default]%s#[fg=mycolor,bg=mycolor]"
-c_short_break="#[fg=mycolor,bg=mycolor]#[fg=red,bold]%s#[fg=mycolor,bg=mycolor]"
-c_long_break="#[fg=mycolor,bg=mycolor]#[fg=red,bold]%s#[fg=mycolor,bg=mycolor]"
+color_session="#[fg=mycolor,bg=mycolor]#[fg=default]%s#[fg=mycolor,bg=mycolor]"
+color_short_break="#[fg=mycolor,bg=mycolor]#[fg=red,bold]%s#[fg=mycolor,bg=mycolor]"
+color_long_break="#[fg=mycolor,bg=mycolor]#[fg=red,bold]%s#[fg=mycolor,bg=mycolor]"
 
-s_start_session="Session started"
-s_start_short_break="Start short break"
-s_stop_short_break="Stop short break"
-s_start_long_break="Start long break"
-s_stop_long_break="Stop long break"
+msg_start_session="Session started"
+msg_start_short_break="Start short break"
+msg_stop_short_break="Stop short break"
+msg_start_long_break="Start long break"
+msg_stop_long_break="Stop long break"
 
-f_tmux_pipe=$HOME/.pomux-tmux
-f_lock_file=$HOME/.pomux-lock
-n_exe="notify-send"
-n_short_breaks_tot=4
-n_length_pb=20
-b_enable_pb=1
+file_tmux_pipe=$HOME/.pomux-tmux
+file_lock=$HOME/.pomux-lock
 
-t_s_session=$((t_m_session * 60))
-t_s_short_break=$((t_m_short_break * 60))
-t_s_long_break=$((t_m_long_break * 60))
+notify_exe="notify-send"
 
-[ -f $f_lock_file ] && exit 0
-trap "{ rm -f $f_tmux_pipe $f_lock_file; exit 255; }" EXIT INT
-echo $$ > $f_lock_file
+num_short_breaks=4
+
+pb_length=20
+pb_enable=1
+
+[ -f $file_lock ] && exit 0
+trap "{ rm -f $file_tmux_pipe $file_lock; exit 255; }" EXIT INT
+echo $$ > $file_lock
 
 wait_for_usr2()
 {
 	local catch=0
 	trap "catch=1" USR2
-	while [ $catch -eq 0 ]; do
+	while [ "$catch" -eq 0 ]; do
 		sleep 1
 	done
 	trap - USR2
@@ -46,21 +45,22 @@ s_to_m()
 
 progress_bar()
 {
-	local n_cur
-	n_cur=$(($1 * $3 / $2))
-	n_cur=$(($3 - $n_cur))
+	local time_left=$1
+	local time_tot=$2
+	local pb_len=$3
+	local n_cur=$(($pb_len - ($time_left * $pb_len / $time_tot)))
 
-	local x=1
+	local cnt=1
 
-	printf "%2d%% [" "$(($1 * 100 / $2))"
-	while [ $x -le $3 ]; do
-		if [ $x -le $n_cur ]; then
+	printf "%2d%% [" "$(($time_left * 100 / $time_tot))"
+	while [ "$cnt" -le "$pb_len" ]; do
+		if [ "$cnt" -le "$n_cur" ]; then
 			printf "."
 		else
 			printf "="
 		fi
 
-		x=$((x + 1))
+		cnt=$((cnt + 1))
 	done
 	printf "]"
 }
@@ -76,44 +76,44 @@ print_status()
 
 	local str=""
 
-	if [ $pb_enable	-ne 0 ]; then
-		str=$(progress_bar $time_left $time_tot $pb_len)
-	fi
+	[ "$pb_enable" -ne 0 ] && str=$(progress_bar $time_left $time_tot $pb_len)
 	str="$str $(s_to_m $time_left)"
 	str="$str $step"
 
 	printf $color "$str"
 }
 
+time_session_sec=$((time_session_min * 60))
+time_short_break_sec=$((time_short_break_min * 60))
+time_long_break_sec=$((time_long_break_min * 60))
+
 step="P"
-t_s=$t_s_session
-t_s_tot=$t_s_session
-c_s=$c_session
+cur_step_sec=$time_session_sec
+cur_step_sec_tot=$time_session_sec
+cur_step_color=$color_session
 n_short_breaks=0
 
 while true
 do
-	t_s=$((t_s - 1))
-	print_status $step $c_s $t_s $t_s_tot $n_length_pb $b_enable_pb > $f_tmux_pipe
+	cur_step_sec=$((cur_step_sec - 1))
+	print_status $step $cur_step_color $cur_step_sec $cur_step_sec_tot $pb_length $pb_enable > $file_tmux_pipe
 
 	sleep 1
 
-	if [ $t_s -eq 0 ]; then
+	if [ "$cur_step_sec" -eq 0 ]; then
 
 		case "$step" in
 		P)
-			if [ $n_short_breaks -eq $n_short_breaks_tot ]; then
-				$n_exe "$s_start_long_break"
-				t_s=$t_s_long_break
-				t_s_tot=$t_s_long_break
-				c_s=$c_long_break
+			if [ "$n_short_breaks" -eq "$num_short_breaks" ]; then
+				[ ! -z "$notify_exe" -a ! -z "$msg_start_long_break" ] && $notify_exe "$msg_start_long_break"
+				cur_step_sec_tot=$time_long_break_sec
+				cur_step_color=$color_long_break
 				step="LB"
 				n_short_breaks=0
 			else
-				$n_exe "$s_start_short_break"
-				t_s=$t_s_short_break
-				t_s_tot=$t_s_short_break
-				c_s=$c_short_break
+				[ ! -z "$notify_exe" -a ! -z "$msg_start_short_break" ] && $notify_exe "$msg_start_short_break"
+				cur_step_sec_tot=$time_short_break_sec
+				cur_step_color=$color_short_break
 				step="SB"
 			fi
 			;;
@@ -121,19 +121,20 @@ do
 		SB | LB)
 			if [ "$step" = "SB" ]; then
 				n_short_breaks=$((n_short_breaks + 1))
-				$n_exe "$s_stop_short_break"
+				[ ! -z "$notify_exe" -a ! -z "$msg_stop_short_break" ] && $notify_exe "$msg_stop_short_break"
 			else
-				$n_exe "$s_stop_long_break"
+				[ ! -z "$notify_exe" -a ! -z "$msg_stop_long_break" ] && $notify_exe "$msg_stop_long_break"
 			fi
 
-			t_s=$t_s_session
-			t_s_tot=$t_s_session
-			c_s=$c_session
+			cur_step_sec_tot=$time_session_sec
+			cur_step_color=$color_session
 			step="P"
 			wait_for_usr2
-			$n_exe "$s_start_session"
+			$notify_exe "$msg_start_session"
 			;;
 		esac
+
+		cur_step_sec=$cur_step_sec_tot
 	fi
 
 done
